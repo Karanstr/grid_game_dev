482296759abe5503dff1518c68779c21b83e40da
Ezra Blasko <reynacalterion@gmail.com>
2024-10-29 12:16:15 -0400

    WHEW. That was a lot. Sorry I went dark for a few days. You know how you have a small change in mind which only requires overhauling the entire system, so you start doing that change, right? Well, then while changing everything you realize everything sucks and you've been an idiot the way you're handling errors, so you implement proper rust error handling. Then you forget why you were making the change, about to undo it when you realize the change was genius but didn't go far enough, so you actually have to change EVERYTHING instead of just everything, and now we're here. Absolutely gaming. (Actual changes: 1. Implemented proper return typing and handling using Options<> and Results<>. 2. Addressed issue #8, but in a crazy way. Instead of adding some dumb checks and jumping layers, I killed layers. The entire system is now one single clump of data which points around to eachother. Trust me, it's genius. 3. I moved the reference checker into it's own struct for simplicity. 4. I broke the tree-cartesian-binarizer but don't worry, it'll be fixed soon. Rendering and all that will be fixed once I fix the binarizer.) I think thats everything. ((Sorry this is terrible practice for a commit message but I don't have a blog to brag yet and I had to get that all out somewhere. Returning to your regular sized, boring commit messages from here on out. (Probably) ))

---
+++ src/graph.rs
@@ -0,0 +1,305 @@
+use derive_more::{Deref, Index as Indexable, IndexMut as IndexableMut};
+use macroquad::math::*;
+//Figure out modules and libraries and all that. I've no clue how they work
+use crate::garbagetracker::{ReferenceTracker, ReferenceStatus}; 
+
+#[derive(Debug)]
+pub struct Node {
+    pub child_indexes:Vec<usize>,
+    pub gc:ReferenceTracker,
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other:&Self) -> bool {
+        self.child_indexes == other.child_indexes
+    }
+}
+
+impl Node {
+    fn new(child_count:usize, protected:bool) -> Self {
+        Self {
+            child_indexes: vec![0; child_count],
+            //We have to garbage collect ourselves :(
+            gc: ReferenceTracker::new(protected), 
+        }
+    }
+
+    fn clone_without_ref(&self, protected:bool) -> Self {
+        Self {
+            child_indexes: self.child_indexes.clone(),
+            gc: ReferenceTracker::new(protected),
+        }
+    }
+    
+    fn read_child(&self, child_direction:usize) -> Index {
+        Index(self.child_indexes[child_direction])
+    }
+}
+
+//Stores dimension for (currently not implemented error prevention)
+#[derive(Debug)]
+pub struct Path {
+    _dimension : usize,
+    directions : Vec<usize>
+} 
+
+impl Path {
+    pub fn from(bit_path:usize, steps:usize, dimension:usize) -> Self {
+        let mut directions:Vec<usize> = Vec::with_capacity(steps);
+        let mut mask = 0; for _ in 0 .. dimension { mask = (mask << 1) | 1 }
+        for step in 0 .. steps {
+            directions.push((bit_path >> (dimension * (steps - step - 1))) & mask);
+        }
+        Self {
+            _dimension : dimension,
+            directions
+        }
+    }
+}
+
+#[derive(Deref, PartialEq, PartialOrd, Clone, Copy)]
+pub struct Index(pub usize);
+
+//For now only supports a graph where each node has four children.
+//This is incredibly arbitrary and incredibly easy to change
+#[derive(Indexable, IndexableMut)]
+pub struct SparseDirectedGraph(Vec<Node>);
+
+impl SparseDirectedGraph {
+
+    pub fn new() -> Self {
+        let empty_node = Node::new(4, true);
+        let mut full_node = Node::new(4, true);
+        for child in 0 .. 4 {
+            full_node.child_indexes[child] = 1;
+        }
+        Self(vec![empty_node, full_node])
+    }
+
+    pub fn get_empty_root(&mut self) -> Index {
+        Index(0)
+    }
+
+
+    //Private methods used to read from the graph
+    fn get_mut_node(&mut self, index:Index) -> Option<&mut Node> {
+        //If the index is out of range or in a protected slot
+        if index > self.last_index() || self[*index].gc.get_status() == ReferenceStatus::Protected {
+            None
+        } else {
+            Some(&mut self[*index])
+        }
+    }
+
+    fn read_child(&self, index:Index, child_direction:usize) -> Option<Index> {
+        Some(self.get_node(index)?.read_child(child_direction))
+    }
+
+    fn find_node(&self, node:&Node) -> Option<Index> {
+        for cur_index in 0 ..= *self.last_index() {
+            let index = Index(cur_index);
+            let cur_node = self.get_node(index)?;
+            if node == cur_node && cur_node.gc.get_status() != ReferenceStatus::Protected {
+                return Some(index);
+            }
+        }
+        None
+    }
+
+    fn get_or_make_empty_index(&mut self) -> Index {
+        let empty_node = Node::new(4, false);
+        match self.find_node(&empty_node) {
+            Some(index) => index,
+            None => {
+                self.0.push(empty_node);
+                self.last_index()
+            }
+        }
+    }
+
+    fn get_trail(&self, root:Index, path:&Path) -> Vec<Index> {
+        let mut trail:Vec<Index> = vec![root];
+        for step in 0 .. path.directions.len() - 1 {
+            match self.read_child(trail[step], path.directions[step]) {
+                Some(child_index) if child_index != trail[step] => trail.push(child_index),
+                _ => break
+            };
+        }
+        trail
+    }
+
+    pub fn last_index(&self) -> Index {
+        Index(self.0.len() - 1)
+    }
+
+
+    //Public methods used to read from the graph
+    pub fn get_node(&self, index:Index) -> Option<&Node> {
+        if index > self.last_index() { 
+            None 
+        } else {
+            Some(&self[*index])
+        }
+    }
+
+    pub fn read_destination(&self, root:Index, path:&Path) -> Option<Index> {
+        let trail = self.get_trail(root, path);
+        self.read_child(*trail.last()?, path.directions[trail.len() - 1])
+    }
+
+
+    //Private methods used to modify graph data
+    fn free_node(&mut self, index:Index) {
+        self[*index] = Node::new(4, false);
+    }
+
+    fn dec_ref_count(&mut self, index:Index) {
+        let mut stack:Vec<Index> = Vec::new();
+        stack.push( index );
+        while stack.len() != 0 {
+            let cur_index = stack.pop().unwrap();
+            if let Some(node) = self.get_mut_node(cur_index) {
+                match node.gc.modify_ref(-1) {
+                    Ok(status) => if let ReferenceStatus::Zero = status {
+                        for child_direction in 0 .. 4 {
+                            stack.push( node.read_child(child_direction) );
+                        }
+                        self.free_node(cur_index);
+                    },
+                    Err(message) => eprintln!("{message}")
+                }
+            }
+        }
+    }
+
+    fn inc_ref_count(&mut self, index:Index) {
+        match self.get_mut_node(index) {
+            Some(node) => {_ = node.gc.modify_ref(1)},
+            None => ()
+        };
+    }
+
+    fn add_node(&mut self, node:Node) -> Index {
+        match self.find_node(&node) {
+            Some(index) => index,
+            None => {
+                let index = self.get_or_make_empty_index();
+                for child_direction in 0 .. 4 {
+                    self.inc_ref_count(node.read_child(child_direction));
+                }
+                self[*index] = node;
+                index
+            }
+        }
+    }
+
+    fn transfer_reference(&mut self, giver:Index, reciever:Index) {
+        self.inc_ref_count(reciever);
+        self.dec_ref_count(giver);
+    }
+
+
+    //Public methods used to modify graph data
+    pub fn set_node_child(&mut self, root:Index, path:&Path, child:Index) -> Index {
+        let trail = self.get_trail(root, path);
+        let mut new_index = child;
+        let steps = path.directions.len() - 1;
+        for step in 0 ..= steps {
+            let mut node = if steps - step < trail.len() && trail[steps - step] != root {
+                match self.get_node(trail[steps - step]) {
+                    Some(node) => node.clone_without_ref(false),
+                    None => Node::new(4, false)
+                }
+            } else { Node::new(4, false) };
+            node.child_indexes[path.directions[steps - step]] = *new_index;
+            new_index = self.add_node(node);
+        }
+        self.transfer_reference(root, new_index);
+        new_index
+    }
+
+
+}
+
+//Figure out where to put this
+
+
+    
+    /* 
+    Not 2d/dimensionless yet. This is the next step.
+     - Figure out where to put them, they don't feel like they belong in such a generalized graph.
+     - Make them functional with the new graph layout. Should be simple..?
+     - Generalize them to n dimensions, along with the rest of the graph.
+    */
+    /*
+
+    fn _compact_root_children(&mut self, root:&mut NodeAddress) -> bool {
+        let child_directions = [1, 0];
+        let mut new_root_node = Node::new_empty();
+        let children = self.get_node(&root).child_indexes;
+        for index in 0 .. child_directions.len() {
+            let address = NodeAddress::new(root.layer - 1, children[index]);
+            let node = self.get_node(&address);
+            let (child_count, last_index) = node.count_kids_and_get_last();
+            if child_count > 1 || last_index != child_directions[index] {
+                return false //Cannot compact root
+            }
+            new_root_node.child_indexes[index] = node.child_indexes[child_directions[index]];
+        } //If we don't terminate we are safe to lower the root
+        let new_root_index = self.add_node(root.layer - 1, new_root_node);
+        self.transfer_reference(&root, &NodeAddress::new(root.layer - 1, new_root_index));
+        root.layer -= 1;
+        root.index = new_root_index;
+        true //Successfully compacted root
+    }
+
+
+    //Only works with 2 dimensions, up to root level 5 (64x64 cell area)
+    pub fn df_to_bin_grid(&self, root:usize) -> Vec<u64> {
+        let blocks_per_side = 2usize.pow(1 + root.layer as u32);
+        let mut bin_grid:Vec<u64> = Vec::new();
+        bin_grid.resize(blocks_per_side, 0);
+
+        //Storing node addresses and their z-order cell
+        let mut stack: Vec<(NodeAddress, u32)> = Vec::new();
+        stack.push( (root.clone(), 0) );
+
+        //Figure out how to make this thread-compatible soon
+        while stack.len() > 0 {
+            let (cur_address, cur_z_order) = stack.pop().unwrap();
+            //Should be safe to unwarp here.
+            let cur_node = match self.get_node(&cur_address) {
+                Some(node) => node,
+                None => continue
+            };
+
+            for child in 0 .. cur_node.child_indexes.len() {
+                let child_index = cur_node.child_indexes[child];
+                let kid_z_order = (cur_z_order << 2) | child as u32;
+                if cur_node.is_child_leaf(child) {
+                    if child_index == 0 { continue }
+                    let cartesian = zorder_to_cartesian(kid_z_order, root.layer);
+                    bin_grid[cartesian.y as usize] |= 1 << cartesian.x;
+                } else {
+                    stack.push( (NodeAddress::new(cur_address.layer - 1, child_index), kid_z_order) );
+                }
+            }
+        }
+        bin_grid
+    } 
+
+
+    pub fn zorder_to_cartesian(mut cell:u32, root_layer:usize) -> UVec2 {
+        let mut xy = UVec2::new(0, 0);
+        for layer in 0 ..= root_layer {
+            xy.x |= (cell & 0b1) << layer;
+            cell >>= 1;
+            xy.y |= (cell & 0b1) << layer;
+            cell >>= 1;
+        }
+        xy
+    }
+
+
+    
+     */
--- src/main.rs
+++ src/main.rs
@@ -1,144 +1,141 @@
 use macroquad::prelude::*;
-mod sddag;
-use sddag::{NodeAddress, Path, SparseDimensionlessDAG};
-
-const BOXSIZE:Vec2 = Vec2::splat(20.);
-
-
-struct Object {
-    root : NodeAddress,
-    position : Vec2,
-}
+use graph::{SparseDirectedGraph, Index, Path};
+mod graph;
+mod garbagetracker;
 
 
-impl Object {
-
-    fn render(&self, dag:&SparseDimensionlessDAG) {
-        let blocks_on_side = 2u32.pow(self.root.layer as u32);
-        let cell_count = (blocks_on_side*2).pow(2);
-        //Probably a better way to do this, but I don't care. Rendering is not currently the bottleneck, and optimizing early will just waste time.
-        for cell in 0 .. cell_count {
-            let cur_cell = dag.read_node_child(
-                &self.root, 
-                0, 
-                &Path::from(cell, self.root.layer + 1, 2)
-            );
-            let color = if cur_cell == 0 { RED } else { BLUE };
-
-            let cartesian_cell = zorder_to_cartesian(cell, self.root.layer);
-            let offset = (Vec2::new(cartesian_cell.x as f32, cartesian_cell.y as f32) - Vec2::splat(blocks_on_side as f32)) * BOXSIZE;
-    
-            draw_rectangle(
-                self.position.x + offset.x,
-                self.position.y + offset.y, 
-                BOXSIZE.x, 
-                BOXSIZE.y, 
-                color);
-        }
-        //Draw center of box
-        draw_rectangle(self.position.x - 5., self.position.y - 5., 10., 10., GREEN);
-    }
-
-    fn move_with_wasd(&mut self, speed:f32) {
-        if is_key_down(KeyCode::A) {
-            self.position.x -= speed;
-        }
-        if is_key_down(KeyCode::D) {
-            self.position.x += speed;
-        }
-        if is_key_down(KeyCode::W) {
-            self.position.y -= speed;
-        }
-        if is_key_down(KeyCode::S) {
-            self.position.y += speed;
-        }
-    }
-
-    fn toggle_cell_with_mouse(&mut self, sddag:&mut SparseDimensionlessDAG, mouse_pos:Vec2) {
-        let rel_mouse_pos = mouse_pos - self.position;
-        let unrounded_cell = rel_mouse_pos / BOXSIZE;
-        let mut edit_cell = IVec2::new(
-            round_away_0_pref_pos(unrounded_cell.x),
-            round_away_0_pref_pos(unrounded_cell.y)
-        );
-        let blocks_on_side = 2i32.pow(self.root.layer as u32);
-        if edit_cell.abs().max_element() > blocks_on_side { return }
-        edit_cell += blocks_on_side;
-        if edit_cell.x > blocks_on_side { edit_cell.x -= 1 }
-        if edit_cell.y > blocks_on_side { edit_cell.y -= 1 }
-
-        let cell = cartesian_to_zorder(edit_cell.x as u32, edit_cell.y as u32, self.root.layer);
-        let path = Path::from(cell, self.root.layer + 1, 2);
-        let cur_node_val = sddag.read_node_child(&self.root, 0, &path);
-        let new_val = if cur_node_val == 0 { 1 } else { 0 } as usize;
-        sddag.set_node_child(&mut self.root, 0, &path, new_val);
-    }
-
-
-}
+// const BOXSIZE:Vec2 = Vec2::splat(40.);
 
 
 #[macroquad::main("First Window")]
 async fn main() {
 
-    let mut sddag = SparseDimensionlessDAG::new(2);
+    let mut sparse_graph: SparseDirectedGraph = SparseDirectedGraph::new();
+    let mut root = sparse_graph.get_empty_root();
 
-    let mut player = Object {
-        root : NodeAddress::new(2, 0),
-        position : Vec2::new(screen_width()/2., screen_height()/2.),
-    };
+    let new_path = Path::from(0b11, 1, 2);
+    root = sparse_graph.set_node_child(root, &new_path, Index(1));
+    let new_path = Path::from(0b00, 1, 2);
+    root = sparse_graph.set_node_child(root, &new_path, root);
+    root = sparse_graph.set_node_child(root, &new_path, Index(1));
+    dbg!(*root);
 
     //Keeps window alive
     loop {
-
-        if is_mouse_button_pressed(MouseButton::Left) {
-            player.toggle_cell_with_mouse(&mut sddag, Vec2::from(mouse_position()));
-            //dbg!(sddag.df_to_bin_grid(&player.root));
-        } 
-
-        player.move_with_wasd(5.);
-
-        player.render(&sddag);
+       
 
         next_frame().await
     }
-        
 
-        
 }
 
+//Update all this object stuff to work with the new (gamer) graph layout
+// struct Object {
+//     root : NodeAddress,
+//     position : Vec2,
+// }
 
-//Figure out where to put these
-fn round_away_0_pref_pos(number:f32) -> i32 {
-    if number < 0. {
-        number.floor() as i32
-    } else if number > 0. {
-        number.ceil() as i32
-    }
-    else {
-        //We don't want to return 0 when we're trying to avoid 0
-        //And the name of the function is prefer_position, so..
-        1 
-    }
-}
+// impl Object {
 
-pub fn zorder_to_cartesian(mut cell:u32, root_layer:usize) -> UVec2 {
-    let mut xy = UVec2::new(0, 0);
-    for layer in 0 ..= root_layer {
-        xy.x |= (cell & 0b1) << layer;
-        cell >>= 1;
-        xy.y |= (cell & 0b1) << layer;
-        cell >>= 1;
-    }
-    xy
-}
+//     fn render(&self, dag:&SparseDimensionlessDAG) {
+//         let blocks_on_side = 2usize.pow(self.root.layer as u32);
+//         let cell_count = (blocks_on_side*2).pow(2);
+//         //Probably a better way to do this, but I don't care. Rendering is not currently the bottleneck, and optimizing early will just waste time.
+//         for cell in 0 .. cell_count {
+//             let path = Path::from(cell, self.root.layer + 1, 2);
+//             let color = match dag.read_end_of_path(&self.root, &path) {
+//                 Some(val) => if val == 0 { RED} else { BLUE },
+//                 None => RED
+//             };
+//             let cartesian_cell = zorder_to_cartesian(cell, self.root.layer) - blocks_on_side as i32;
+//             let offset = Vec2::new(cartesian_cell.x as f32, cartesian_cell.y as f32) * BOXSIZE;
+    
+//             draw_rectangle(
+//                 self.position.x + offset.x,
+//                 self.position.y + offset.y, 
+//                 BOXSIZE.x, 
+//                 BOXSIZE.y, 
+//                 color);
+//         }
+//         //Draw center of box
+//         draw_rectangle(self.position.x - 5., self.position.y - 5., 10., 10., GREEN);
+//     }
 
-pub fn cartesian_to_zorder(x:u32, y:u32, root_layer:usize) -> u32 {
-    let mut cell = 0;
-    for layer in (0 ..= root_layer).rev() {
-        let step = (((y >> layer) & 0b1) << 1 ) | ((x >> layer) & 0b1);
-        cell = (cell << 2) | step;
-    }
-    cell
-}
+//     fn move_with_wasd(&mut self, speed:f32) {
+//         if is_key_down(KeyCode::A) {
+//             self.position.x -= speed;
+//         }
+//         if is_key_down(KeyCode::D) {
+//             self.position.x += speed;
+//         }
+//         if is_key_down(KeyCode::W) {
+//             self.position.y -= speed;
+//         }
+//         if is_key_down(KeyCode::S) {
+//             self.position.y += speed;
+//         }
+//     }
+
+//     fn toggle_cell_with_mouse(&mut self, sddag:&mut SparseDimensionlessDAG, mouse_pos:Vec2) {
+//         let rel_mouse_pos = mouse_pos - self.position;
+//         let unrounded_cell = rel_mouse_pos / BOXSIZE;
+//         let mut edit_cell = IVec2::new(
+//             round_away_0_pref_pos(unrounded_cell.x),
+//             round_away_0_pref_pos(unrounded_cell.y)
+//         );
+//         let blocks_on_side = 2i32.pow(self.root.layer as u32);
+//         if edit_cell.abs().max_element() > blocks_on_side { return }
+//         edit_cell += blocks_on_side;
+//         if edit_cell.x > blocks_on_side { edit_cell.x -= 1 }
+//         if edit_cell.y > blocks_on_side { edit_cell.y -= 1 }
+
+//         let cell = cartesian_to_zorder(edit_cell.x as usize, edit_cell.y as usize, self.root.layer);
+//         let path = Path::from(cell, self.root.layer + 1, 2);
+//         let cur_val = match sddag.read_end_of_path(&self.root, &path) {
+//             Some(value) => value,
+//             None => 0
+//         };
+//         let new_val = if cur_val == 0 { 1 } else { 0 };
+//         sddag.set_node_child(&mut self.root, &path, new_val, true);
+//         dbg!(sddag.df_to_bin_grid(&self.root));
+//     }
+
+// }
+
+
+// //Figure out where to put these
+// fn round_away_0_pref_pos(number:f32) -> i32 {
+//     if number < 0. {
+//         number.floor() as i32
+//     } else if number > 0. {
+//         number.ceil() as i32
+//     }
+//     else {
+//         //We don't want to return 0 when we're trying to avoid 0
+//         //And the name of the function is prefer_position, so..
+//         1 
+//     }
+// }
+
+// //Will overflow if our z-order goes 32 layers deep. So.. don't do that
+// pub fn zorder_to_cartesian(cell:usize, root_layer:usize) -> IVec2 {
+//     let mut u32_cell = cell as i32;
+//     let (mut x, mut y) = (0, 0);
+//     for layer in 0 ..= root_layer {
+//         x |= (u32_cell & 0b1) << layer;
+//         u32_cell >>= 1;
+//         y |= (u32_cell & 0b1) << layer;
+//         u32_cell >>= 1;
+//     }
+//     IVec2::new(x, y)
+// }
+
+// pub fn cartesian_to_zorder(x:usize, y:usize, root_layer:usize) -> usize {
+//     let mut cell = 0;
+//     for layer in (0 ..= root_layer).rev() {
+//         let step = (((y >> layer) & 0b1) << 1 ) | ((x >> layer) & 0b1);
+//         cell = (cell << 2) | step;
+//     }
+//     cell
+// }
 
