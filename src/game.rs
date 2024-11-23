3f19fa000d5b6a72b07a041f4e6aa112f3aa2c00
Ezra Blasko <reynacalterion@gmail.com>
2024-11-22 22:08:38 -0500

    Extracted basic raymarching stuff into a Particle class. Next comes figuring out how to store the local root (and offset from that local root instead of global position

--- src/game.rs
+++ src/game.rs
@@ -17,6 +17,99 @@ enum BlockType {
     //...
 }
 
+#[derive(Debug)]
+struct Particle {
+    position : Vec2,
+    velocity : Vec2,
+    configuration : u8,
+}
+
+impl Particle {
+
+    fn slide_check(&self, object:&Object, mut walls_hit:IVec2, position_data:[Option<(BlockType, UVec2, u32)>; 4]) -> IVec2 {
+        //Formalize this with some zorder arithmatic.
+        let (x_slide_check, y_slide_check) = if self.velocity.x < 0. && self.velocity.y < 0. { //(-,-)
+            (2, 1)
+        } else if self.velocity.x < 0. && self.velocity.y > 0. { //(-,+)
+            (0, 3)
+        } else if self.velocity.x > 0. && self.velocity.y < 0. { //(+,-)
+            (3, 0)
+        } else { //(+,+)
+            (1, 2)
+        };
+        let x_block_collision = match position_data[x_slide_check] {
+            Some((block, ..)) => object.on_touch(block),
+            None => OnTouch::Resist(IVec2::ZERO)
+        };
+        let y_block_collision = match position_data[y_slide_check] {
+            Some((block, ..)) => object.on_touch(block),
+            None => OnTouch::Resist(IVec2::ZERO)
+        };
+        if x_block_collision != y_block_collision {
+            if let OnTouch::Resist(_) = x_block_collision { walls_hit.x = 0 }
+            if let OnTouch::Resist(_) = y_block_collision { walls_hit.y = 0 }
+        }
+        walls_hit
+    }
+
+    fn next_boundary_in_tick(&mut self, object:&Object, graph:&SparseDirectedGraph, max_depth:u32, first:bool) -> Option<OnTouch> {
+        let relevant_cell = Zorder::from_configured_direction(self.velocity, self.configuration);
+        let (cur_block_type, mut grid_cell, mut cur_depth) = object.get_data_at_position(graph, self.position, max_depth)[
+            if first { Zorder::from_configured_direction(-self.velocity, self.configuration) } else {relevant_cell}
+        ]?;
+        loop {
+            let (new_position, ticks_to_reach, walls_hit) = object.next_intersection(grid_cell, cur_depth, self.position, self.velocity);
+            dbg!(new_position);
+            if ticks_to_reach >= 1. { return None }
+            self.velocity -= new_position - self.position;
+            self.position = new_position;
+            let data = object.get_data_at_position(graph, self.position, max_depth);
+            let new_block_type;
+            (new_block_type, grid_cell, cur_depth) = data[relevant_cell]?;
+            if new_block_type == cur_block_type { continue }
+            return match object.on_touch(new_block_type) {
+                OnTouch::Ignore => Some(OnTouch::Ignore),
+                OnTouch::Resist(_) => {
+                    Some(OnTouch::Resist(
+                        if walls_hit.x == walls_hit.y {
+                            self.slide_check(object, walls_hit, data)
+                        } else { walls_hit }
+                    ))
+                },
+            }
+        }
+    }
+
+    fn march_through(&mut self, object:&Object, graph:&SparseDirectedGraph, max_depth:u32) {
+        let mut velocity = self.velocity;
+        let mut first = true;
+        while self.velocity.length() != 0. {
+            match self.next_boundary_in_tick(object, graph, max_depth, first) {
+                Some(action) => {
+                    match action {
+                        OnTouch::Ignore => first = false,
+                        OnTouch::Resist(walls_hit) => {
+                            first = true;
+                            if walls_hit.x == 1 {
+                                self.velocity.x = 0.;
+                                velocity.x = 0.;
+                            }
+                            if walls_hit.y == 1 {
+                                self.velocity.y = 0.;
+                                velocity.y = 0.;
+                            }
+                        }
+                    }
+                },
+                None => break
+            }
+        }
+        self.position += self.velocity;
+        self.velocity = velocity;
+    }
+
+}
+
 pub struct Object {
     root : NodePointer,
     position : Vec2,
@@ -111,96 +204,7 @@ impl Object {
         }
         data
     }
-
-    fn slide_check(&self, walls_hit:IVec2, displacement:Vec2, position_data:[Option<(BlockType, UVec2, u32)>; 4]) -> IVec2 {
-        let mut updated_walls = walls_hit;
-        //Formalize this by adding a zorder struct with all the zorder methods and some zorder arithmatic.
-        let (x_slide_check, y_slide_check) = if displacement.x < 0. && displacement.y < 0. { //(-,-)
-            (2, 1)
-        } else if displacement.x < 0. && displacement.y > 0. { //(-,+)
-            (0, 3)
-        } else if displacement.x > 0. && displacement.y < 0. { //(+,-)
-            (3, 0)
-        } else { //(+,+)
-            (1, 2)
-        };
-        let x_block_collision = match position_data[x_slide_check] {
-            Some((block, ..)) => self.on_touch(block),
-            None => OnTouch::Resist(IVec2::ZERO)
-        };
-        let y_block_collision = match position_data[y_slide_check] {
-            Some((block, ..)) => self.on_touch(block),
-            None => OnTouch::Resist(IVec2::ZERO)
-        };
-        if x_block_collision != y_block_collision {
-            if let OnTouch::Resist(_) = x_block_collision { updated_walls.x = 0 }
-            if let OnTouch::Resist(_) = y_block_collision { updated_walls.y = 0 }
-        }
-        updated_walls
-    }
-
-    //Add the option for no orientation, making my life difficult in exchange for functionality
-    //Add particle struct, containing position, velocity, configuration
-    fn next_boundary_in_tick(&self, graph:&SparseDirectedGraph, position:Vec2, displacement:Vec2, configuration:u8, max_depth:u32, first:bool) -> Option<(Vec2, OnTouch)> {
-        let relevant_cell = Zorder::from_configured_direction(displacement, configuration);
-        let (mut cur_position, mut rem_displacement) = (position, displacement);
-        let (cur_block_type, mut grid_cell, mut cur_depth) = self.get_data_at_position(graph, position, max_depth)[
-            if first { Zorder::from_configured_direction(-displacement, configuration) } else {relevant_cell}
-        ]?;
-        loop {
-            let (new_position, ticks_to_reach, walls_hit) = self.next_intersection(grid_cell, cur_depth, cur_position, rem_displacement);
-            if ticks_to_reach >= 1. { return None }
-            rem_displacement -= new_position - cur_position;
-            cur_position = new_position;
-            let data = self.get_data_at_position(graph, new_position, max_depth);
-            let new_block_type;
-            (new_block_type, grid_cell, cur_depth) = data[relevant_cell]?;
-            if new_block_type == cur_block_type { continue }
-            return match self.on_touch(new_block_type) {
-                OnTouch::Ignore => Some((new_position, OnTouch::Ignore)),
-                OnTouch::Resist(_) => {
-                    Some((new_position, OnTouch::Resist(
-                        if walls_hit.x == walls_hit.y {
-                            self.slide_check(walls_hit, rem_displacement, data)
-                        } else { walls_hit }
-                    )))
-                },
-            }
-        }
-    }
-    
-    //Maybe extract particle raymarching into the particle struct?
-    fn all_collisions(&self, graph:&SparseDirectedGraph, position:Vec2, velocity:Vec2, max_depth:u32) -> (Vec2, Vec2) {
-        let mut cur_position = position;
-        let mut remaining_displacement = velocity;
-        let mut end_velocity = velocity;
-        let mut first = true;
-        while remaining_displacement.length() != 0. {
-            match self.next_boundary_in_tick(graph, cur_position, remaining_displacement, 1, max_depth, first) {
-                Some((new_position, action)) => {
-                    remaining_displacement -= new_position - cur_position;
-                    cur_position = new_position;
-                    match action {
-                        OnTouch::Ignore => first = false,
-                        OnTouch::Resist(walls_hit) => {
-                            first = true;
-                            if walls_hit.x == 1 {
-                                remaining_displacement.x = 0.;
-                                end_velocity.x = 0.;
-                            }
-                            if walls_hit.y == 1 {
-                                remaining_displacement.y = 0.;
-                                end_velocity.y = 0.;
-                            }
-                        }
-                    }
-                },
-                None => break,
-            }
-        }
-        (cur_position + remaining_displacement, end_velocity)
-    }
-
+ 
     pub fn apply_linear_force(&mut self, force:Vec2) {
         self.velocity += force * Vec2::new(self.rotation.cos(), self.rotation.sin());
     }
@@ -243,7 +247,13 @@ impl Scene {
 
     pub fn move_with_collisions(&mut self, moving:&mut Object, hitting:&Object) {
         if moving.velocity.length() != 0. {
-            (moving.position, moving.velocity) = hitting.all_collisions(&self.graph, moving.position, moving.velocity, 5);
+            let mut particle_approximation = Particle {
+                position : moving.position,
+                velocity : moving.velocity,
+                configuration : 0
+            };
+            particle_approximation.march_through(hitting, &self.graph, 5);
+            (moving.position, moving.velocity) = (particle_approximation.position, particle_approximation.velocity);
             let drag = 0.99;
             moving.velocity *= drag;
             let speed_min = 0.005;
@@ -287,12 +297,11 @@ impl Scene {
 
         if let Ok(root) = self.graph.set_node(modified.root, &path, new_child) {
             modified.root = root
-        };
+        } else { error!("Failed to modify cell. Likely means structure is corrupted.") };
     }
 
 }
 
-//Move into utils or something?
 //Can only handle up to depth 16 for now without overflowing
 pub struct Zorder;
 impl Zorder {
@@ -333,13 +342,13 @@ impl Zorder {
         zorder >> (2 * (depth - layer)) & 0b11
     }
 
-    pub fn divergence_depth(zorder_a:u32, zorder_b:u32, depth:u32) -> u32 {
+    pub fn divergence_depth(zorder_a:u32, zorder_b:u32, depth:u32) -> Option<u32> {
         for layer in 1 ..= depth {
             if Self::read(zorder_a, layer, depth) != Self::read(zorder_b, layer, depth) {
-                return layer
+                return Some(layer)
             }
         }
-        0
+        None    
     }
 
     pub fn path(zorder:u32, depth:u32) -> Vec<u32> {
